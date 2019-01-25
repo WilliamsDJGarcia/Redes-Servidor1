adbdadbaab8753fc9bbed13320cb4b107286ed68
WilliamsDJGarcia <43178740+WilliamsDJGarcia@users.noreply.github.com>
2019-01-25 09:06:44 -0600

    Subir archivo Profile(luego)

---
+++ servidor2.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var aplicacion = express()
+
+const net = require('net')
+const server = require('http').Server(aplicacion)
+const socket = require('socket.io')(server)
+
+var HOST = '192.168.0.62'
+var PORT =  4000
+
+// server.listen(PORT, function(){
+//     console.log('Servidor activo ' + PORT + ' : ' + HOST)
+// })
+
+var ser = net.createServer(function(so){
+    //console.log(so)
+    console.log('Usuario nuevo '+ so.remoteAddress+ ' : ' + so.remotePort )
+    so.on('connect', function(){
+        console.log('Nuevo usuario')
+    })
+
+    so.on('data', function(data){
+        console.log(Buffer.from(data, 'hex').toString('utf8'))
+    })
+
+    so.on('close', function(){
+        console.log('usuario desconectado')
+    })
+
+})
+ser.listen(PORT, HOST);
+
+console.log('Node')
---
+++ wificlient.ino
@@ -0,0 +1,69 @@
+#include <ESP8266WiFi.h>
+
+char ssid[] = "moto";
+char password[] = "12345678";
+
+char host[] = "192.168.43.142";
+int httpPort= 3000;
+int httpPort2 = 4000;
+
+boolean verificar=false;
+int contador = 0;
+WiFiClient client;
+
+void setup() 
+{
+    Serial.begin(9600);
+    delay(10);
+    WiFi.begin(ssid, password);
+
+    while(WiFi.status() != WL_CONNECTED){
+      delay(300);
+      Serial.print(". Wifi estatus");
+      }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP() );
+  
+    while(client.connect(host, httpPort) && verificar == false){
+        delay(10);
+        contador = contador + 1;
+        if(contador >= 30){
+          verificar = true;
+          }
+        Serial.println("connection falied 1");
+      }
+      while(!client.connect(host, httpPort2)){
+        Serial.println("connection falied 2");
+        }
+    Serial.println("connection successs");
+
+}
+void loop() {
+
+   while(client.connected()){
+    Serial.println(".");
+    while(client.connect(host, httpPort)){
+      delay(250);
+      Serial.print("onexion exitosa"); 
+      }
+      while(client.connect(host, httpPort2)){
+      delay(250);
+      Serial.print("conexion 2 exitosa"); 
+      }
+    while(!client.connected()){
+        Serial.println(".");
+    while(client.connect(host, httpPort)){
+      delay(250);
+      Serial.print("onexion exitosa"); 
+      }
+      while(client.connect(host, httpPort2)){
+      delay(250);
+      Serial.print("conexion 2 exitosa");
+      }
+      
+    }
+    }
+delay(25);
+}
