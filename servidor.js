0e361184714571345ee0993cd202c592887dd656
WilliamsDJGarcia <43178740+WilliamsDJGarcia@users.noreply.github.com>
2019-01-29 21:04:21 -0600

    change npm start

--- servidor.js
+++ servidor.js
@@ -7,21 +7,21 @@ const socket = require('socket.io')(server)
 const os = require('os')
 
 
-// var HOST = '192.168.0.62'
-var PORT =  3000
+var HOST = 'https://hidden-fjord-71164.herokuapp.com/'
+// var PORT =  process.env.PORT
 var interface = os.networkInterfaces();
 var ipdinamic;
 
-for(var k in interface){
-    for(var k2 in interface[k]){
-        var address = interface[k][k2]
-        if(address.family == 'IPv4' && !address.internal){
-            ipdinamic = address.address.toString();
-        }
+// for(var k in interface){
+//     for(var k2 in interface[k]){
+//         var address = interface[k][k2]
+//         if(address.family == 'IPv4' && !address.internal){
+//             ipdinamic = address.address.toString();
+//         }
         
-        console.log(ipdinamic);
-    }
-};
+//         console.log(ipdinamic);
+//     }
+// };
 
 
 // server.listen(PORT, function(){
@@ -44,6 +44,10 @@ var ser = net.createServer(function(so){
     })
 
 })
-ser.listen(PORT, address);
+// if(PORT == null || PORT == ''){
+//     PORT = 3000;
+// }
+
+ser.listen(HOST);
 
 console.log('Node');
