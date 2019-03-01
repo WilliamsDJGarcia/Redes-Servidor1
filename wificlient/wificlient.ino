11b42eb821f3ba6392670af032a6cdbe72e4b073
BraulioRuiz <43392482+BraulioRuiz@users.noreply.github.com>
2019-02-28 22:13:19 -0600

    Update wificlient.ino

--- wificlient/wificlient.ino
+++ wificlient/wificlient.ino
@@ -4,15 +4,9 @@ char ssid[] = "moto";
 char password[] = "comatose2019";
 
 char host[] = "redespolitecnica5.ddns.net";
-//<<<<<<< HEAD:wificlient.ino
 int httpPort= 4000;
 int httpPort2 = 5000;
-int hhtpPort3 = 6000;
-//=======
-//int httpPort= 6200;
-//int httpPort2 = 4500;
-//int httpPort3 = 5100;
-//>>>>>>> 4dbcafcde0f01460495fa8f34c901d9624d069b6:wificlient/wificlient.ino
+int httpPort3 = 6000;
 
 boolean verificar=false;
 int contador = 0;
@@ -33,57 +27,69 @@ void setup()
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP() );
   
-    while(client.connect(host, httpPort) && verificar == false){
-        delay(10);
-        contador = contador + 1;
-        if(contador >= 30){
-          verificar = true;
-          }
+    while(!client.connect(host, httpPort)){
+        delay(10);     
         Serial.println("connection falied 1");
+         if(client.connect(host, httpPort2)){
+            Serial.println("Connected to server2");
+            client.write("server 2");
+            Serial.println(client.readString());
+            delay(250);
+            break;
+          }
+          if(client.connect(host, httpPort3)){
+            Serial.println("Connected to server3");
+            client.write("server 3");
+            Serial.println(client.readString());
+            delay(250);
+            break;
+          }
+      Serial.println("Failed to connect server2");
       }
-      while(!client.connect(host, httpPort2)){
-        Serial.println("connection falied 2");
-        }
+      if(client.connect(host, httpPort)){
+        Serial.println("Connected to server1");
+        client.write("server 1");
+        Serial.println(client.readString());
+        delay(250);  
+      }
     Serial.println("connection successs");
 
 }
+bool band = false;
 void loop() {
-
-   while(client.connected()){
+  
+    while(client.connected()){
     Serial.println(".");
-    while(client.connect(host, httpPort)){
-      delay(250);
-      client.write("Server 1")
-      Serial.print("conexion exitosa"); 
-      }
-      while(client.connect(host, httpPort2)){
-      client.write("Server 2")
-      delay(250);
-      Serial.print("conexion 2 exitosa"); 
-      }    
-      while(client.connect(host, httpPort3)){
-      client.write("Server 3")
-      delay(250);
-      Serial.print("conexion 3 exitosa"); 
-      }    
+    delay(500);
     }
-    while(!client.connected()){
-      Serial.println(".");
-    while(client.connect(host, httpPort)){
-      client.write("Server 1")
-      delay(250);
-      Serial.print("conexion exitosa"); 
-      }
-      while(client.connect(host, httpPort2)){
-      client.write("Server 2")
-      delay(250);
-      Serial.print("conexion 2 exitosa");
-      }     
-      while(client.connect(host, httpPort3)){
-      client.write("Server 3")
-      delay(250);
-      Serial.print("conexion 3 exitosa");
-      }     
+    if(client.connected() == false){
+      Serial.println("Cambiando de servidor...");
+      band = false;
+      delay(500);
     }
+    while(band == false){
+       while(!client.connect(host, httpPort)){
+        delay(250);      
+        Serial.print("conexion fallida"); 
+         if(client.connect(host, httpPort2)){
+          Serial.println("conexion al servidor 2 exitosa");          
+          delay(500);
+          break;
+        }else{
+          client.connect(host, httpPort3);
+          Serial.println("conexion al servidor 3 exitosa");          
+          delay(500);
+          break;
+        }
+        Serial.println("conexion fallida al servidor 2");
+        }
+        if(!client.connect(host, httpPort2) && !client.connect(host, httpPort3)){
+          client.connect(host, httpPort);
+          Serial.println("conexion al servidor 1 exitosa");            
+          delay(500);
+          band = true;
+        }
+               
+      }   
 delay(25);
 }
